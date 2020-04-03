80de913e39992c63c1109338f3f21f8e1d3c4c8b
brijesh kumar <59088458+anonymousmani@users.noreply.github.com>
2020-04-03 17:07:00 +0530

    Update qr.py

--- qr.py
+++ qr.py
@@ -2,7 +2,7 @@ import qrcode
 import os
 os.system("apt install toilet")
 os.system("clear")
-os.system("toilet -fmono12 -F gay X_PLOITS")
+os.system("toilet -fmono12 -F gay QR-CODE")
 print("    \033[1;36;40m Code made by: \033[1;32;40m Brijesh Kumar")
 print("    \033[1;36;40m Instagram id: \033[1;32;40m www.instagram.com/x_ploits")
 print("    \033[1;36;40m Github      : \033[1;32;40m www.github.com/anonymousmani")
